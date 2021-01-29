3bcf4a8b93d96950d1016e5b754259fb33c74c02
Samanthika Rajapaksha <53019782+samaa9504@users.noreply.github.com>
2021-01-30 03:45:38 +0530

    Home

---
+++ src/app/components/product-and-services/product-and-services.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductAndServicesComponent } from './product-and-services.component';
+
+describe('ProductAndServicesComponent', () => {
+  let component: ProductAndServicesComponent;
+  let fixture: ComponentFixture<ProductAndServicesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductAndServicesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductAndServicesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
